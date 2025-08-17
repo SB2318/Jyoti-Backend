9094df59618a8c59a64c06c0be8c43842730cf25
SB2318 <sb2318@it.jgec.ac.in>
2025-08-17 21:06:16 +0530

    --initial--commit

---
+++ src/main/java/com/app/jyotijgec/jyotiBackend/JyotiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.app.jyotijgec.jyotiBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JyotiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JyotiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/jyotijgec/jyotiBackend/JyotiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.jyotijgec.jyotiBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JyotiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
